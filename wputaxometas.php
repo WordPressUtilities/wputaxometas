9a637b1b3a076bc174ec5556144040958317a8b8
Kevin <kevin@rocher-dev.com>
2017-08-28 20:06:58 +0200

    v 0.18.5
    * Fix notice for inexistant taxonomy

--- wputaxometas.php
+++ wputaxometas.php
@@ -4,7 +4,7 @@
 Plugin Name: WPU Taxo Metas
 Plugin URI: https://github.com/WordPressUtilities/wputaxometas
 Description: Simple admin for taxo metas
-Version: 0.18.4
+Version: 0.18.5
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -14,7 +14,7 @@ License URI: http://opensource.org/licenses/MIT
 defined('ABSPATH') or die(':(');
 
 class WPUTaxoMetas {
-    public $version = '0.18.4';
+    public $version = '0.18.5';
     public $qtranslate = false;
     public $qtranslatex = false;
     public $fields = array();
@@ -49,6 +49,9 @@ class WPUTaxoMetas {
         // Add hook to edit category
         foreach ($this->taxonomies as $taxo) {
             $taxonomy = get_taxonomy($taxo);
+            if (!is_object($taxonomy)) {
+                continue;
+            }
 
             add_filter('manage_edit-' . $taxo . '_columns', array(&$this,
                 'column_title'
